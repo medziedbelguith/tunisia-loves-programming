ba108d589b214402f99adf2a92889d3caa6e401d
medziedbelguith <medziedbelguith@gmail.com>
2022-06-22 19:38:57 +0200

    Adding a new user

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const getUsers=() =>{
-    return [{name:"Montacer",age:27},{name:"Anas",age:25}];
+    return [
+        {name:"Montacer",age:27},
+        {name:"Anas",age:25},
+        {name:"Joe Doe",age:30},
+];
 }
 
 const x=getUsers();
